e72888c58ec77713051d35464357281c45af64ad
Marcelo Marcon <marcelo.marcon@ax4b.com>
2022-04-22 20:25:06 -0400

    upload file completed

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const express = require("express");
+const multer = require("multer");
+const crypto = require("crypto");
 
 const app = express();
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "media/");
+    },
+    filename: function (req, file, cb) {
+        // cb(null, file.originalname);
+        crypto.randomBytes(16, (err, hash) => {
+            if (err) cb(err);
+
+            const filename = `${hash.toString("hex")}-${
+                file.originalname
+            }`;
+            cb(null, filename);
+        });
+    },
+});
+
+const upload = multer({ storage });
+
 app.set("view engine", "ejs");
 
-app.get("/", (req, res, next) => {
+app.get("/", (req, res) => {
     res.render("home");
+});
+
+app.post("/upload", upload.single("file"), (req, res) => {
+    res.send("arquivo enviado");
 });
 
 app.listen(3000, () => {
